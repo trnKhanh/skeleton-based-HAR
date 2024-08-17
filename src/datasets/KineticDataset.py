5d815a4071b65592212deb12e8378debcc4cb3fc
trnKhanh <tnk29082003@gmail.com>
2024-08-17 15:34:10 +0700

    update

--- src/datasets/KineticDataset.py
+++ src/datasets/KineticDataset.py
@@ -87,13 +87,11 @@ class KineticDataset(Dataset):
         T = len(video_info["data"])
         V = 18
         print("$$$")
-        print(T)
 
         # fill data_numpy
         data_numpy = np.zeros((C, T, V, num_person_in))
         for frame_info in video_info["data"]:
             frame_index = frame_info["frame_index"] - 1
-            print(f"\t{frame_index}")
             for m, skeleton_info in enumerate(frame_info["skeleton"]):
                 if m >= num_person_in:
                     break
